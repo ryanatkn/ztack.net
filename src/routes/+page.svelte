6282af9e2f3879d937450de77960ee1886b374db
Ryan Atkinson <mail@ryanatkn.com>
2024-11-20 19:58:48 -0700

    update logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import Svg from '@ryanatkn/fuz/Svg.svelte';
-	import {fuz_logo} from '@ryanatkn/fuz/logos.js';
+	// import Svg from '@ryanatkn/fuz/Svg.svelte';
+	// import {fuz_logo} from '@ryanatkn/fuz/logos.js';
 	import Card from '@ryanatkn/fuz/Card.svelte';
 	import {base} from '$app/paths';
 </script>
@@ -8,7 +8,8 @@
 <main>
 	<section class="box">
 		<header class="box pt_xl">
-			<Svg data={fuz_logo} size="var(--icon_size_xl)" />
+			<!-- <Svg data={fuz_logo} size="var(--icon_size_xl)" /> -->
+			<span class="icon_size_xl2">🌩</span>
 			<h1 class="mt_xl2">ztack</h1>
 		</header>
 		<blockquote>coming soon</blockquote>
